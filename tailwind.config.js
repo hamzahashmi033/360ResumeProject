5b4ba436a8021642d00bbe773d75ab3bd8d5a8c7
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-07-13 23:58:59 -0700

    add fonts and tailwind config setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,12 +8,40 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        Red: "#C92127",
+        Black: "#0B0A0A",
+        White: "#FAFAFA",
+      },
+      fontFamily: {
+        Neuropal: ["var(--neuropal)"],
+        ClashDisplay: ["var(--clashdisplay)"],
+      },
+      screens: {
+        xs: "0px", // Custom xs screen starting from 0px
+        sm: "640px", // Default sm screen starts from 640px in Tailwind
+        md: "768px", // Default md screen starts from 768px in Tailwind
+        lg: "1024px", // Default lg screen starts from 1024px in Tailwind
+        xl: "1280px", // Default xl screen starts from 1280px in Tailwind
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "custom-gradient":
+          "linear-gradient(97.5deg, #3BAEEB -7.64%, #D5C664 103.16%)",
       },
     },
   },
-  plugins: [],
+
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        ".text-gradient": {
+          "background-clip": "text",
+          "-webkit-background-clip": "text",
+          color: "transparent",
+        },
+      };
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    }),
+  ],
 };
